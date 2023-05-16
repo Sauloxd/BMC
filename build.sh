2315d52be145e61b8a0ca1423e16f758d5f33407
Sauloxd <saulotoshi@gmail.com>
2023-05-16 13:00:56 +0000

    Start from scratch

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-IMAGE_NAME=sauloxd/breakable-toy:build
-
-echo Building image $IMAGE_NAME
-
-docker build \
-    --build-arg RUBY_VERSION=3.1.0 \
-    --build-arg PG_MAJOR=14 \
-    -t $IMAGE_NAME . -f Dockerfile.build
