21a60afa32398cdcf5616bcdf511cc15b1869b93
Sauloxd <saulotoshi@gmail.com>
2023-05-17 19:18:33 +0000

    Add Articles Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get 'articles/index'
-  get 'articles', to: "articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  resources :articles
 end
