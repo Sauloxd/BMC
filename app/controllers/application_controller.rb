ce5ee1e67ca0529d35105c414f562ce49329ab62
Sauloxd <saulotoshi@gmail.com>
2023-05-23 13:49:37 +0000

    Remove unecessary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  def authenticate_user!
-    return if current_user
+  # def authenticate_user!
+  #   return if current_user
 
-    redirect_to new_user_session_path
-  end
+  #   redirect_to new_user_session_path
+  # end
 end
