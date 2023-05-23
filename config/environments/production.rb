df6983c89aa0571cfb96565d83c7e3e4aeb4fa94
Sauloxd <saulotoshi@gmail.com>
2023-05-23 11:54:21 +0000

    rails generate devise:install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Custom
+  # Devise config for sending mailer
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
