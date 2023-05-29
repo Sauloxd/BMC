79e567a814dadfcc3cd55e1cc7a0d17464bc729b
Sauloxd <saulotoshi@gmail.com>
2023-05-29 17:29:20 +0000

    Add upload to S3 for avatar image

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ gem 'devise'
 
 gem "tailwindcss-rails", "~> 2.0"
 
+gem "aws-sdk-s3", require: false
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+  has_many :matches, class_name: 'Match', foreign_key: :player_a_1_id
+  has_many :matches, class_name: 'Match', foreign_key: :player_a_2_id
+  has_many :matches, class_name: 'Match', foreign_key: :player_b_1_id
+  has_many :matches, class_name: 'Match', foreign_key: :player_b_2_id
+
   scope :by_email, -> (email) { where("email ILIKE ?", "%#{email}%") }
   scope :in_match_series, -> (match_series_id) { where(id: MatchSeries.find(match_series_id).match_series_participations.pluck(:user_id)) }
 end
