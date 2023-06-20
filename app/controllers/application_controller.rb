2479cd3f43ef648652d1d0901c73271cbd758256
Sauloxd <saulotoshi@gmail.com>
2023-06-20 13:01:05 +0000

    Add helper for avatar url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  # def authenticate_user!
-  #   return if current_user
-
-  #   redirect_to new_user_session_path
-  # end
-
   protect_from_forgery with: :exception, prepend: true
 
   before_action :configure_permitted_parameters, if: :devise_controller?
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def avatar_url(avatar)
+    avatar.present? ? url_for(avatar) : "https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=AA"
+  end  
 end
